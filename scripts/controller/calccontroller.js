aa889fd9cac5af858d4ebf4e2e6e4a9b1a013b5f
igor soares <igorsoaress771@gmail.com>
2018-06-03 17:45:53 -0300

    criação dos metodos get e set

---
+++ scripts/controller/calccontroller.js
@@ -0,0 +1,19 @@
+class CalcController{
+    constructor(){
+         this._displayCalc = "0";
+         this._dataAtual; 
+    }
+    get displayCalc(){
+        return this._displayCalc;
+    }
+    set displayCalc(){
+        this.displayCalc = valor;
+    }
+    get dataAtual(){
+        return this._dataAtual;
+    }
+    set dataAtual(){
+        this.dataAtual = valor;
+    }
+
+}
